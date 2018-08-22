6c1f182798b3201d220160ee63345896a42ee9ba
Mikhail Fokin <mikhail.fokin@hiq.ru>
2018-08-22 18:07:10 +0300

    section 7 lesson 37

--- albums/index.js
+++ albums/index.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+const App = () => (
+  <View>
+    <Header text='test' />
+    <AlbumList />
+  </View>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('albums', () => App);
